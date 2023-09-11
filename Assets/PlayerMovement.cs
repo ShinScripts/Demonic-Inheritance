7fdaffa2554b7fcbde633302e35e11c98598bc01
Shin <shahin02@live.se>
2023-09-11 15:19:49 +0200

    raycast wall check

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,14 +8,46 @@ public class PlayerMovement : MonoBehaviour
     public GameObject parent;
 
 
+    bool ClearToMove(bool forward = true)
+    {
+        RaycastHit hit;
+
+        Physics.Raycast(transform.position, forward ? transform.forward : transform.forward * -1, out hit, increment);
+
+        return !hit.collider;
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
+<<<<<<< Updated upstream
             transform.localPosition += Vector3.forward * increment;
         } else if (Input.GetKeyDown(KeyCode.S))
         {
             transform.localPosition += Vector3.back * increment;
+=======
+
+            if (ClearToMove())
+            {
+                transform.localPosition += transform.forward * increment;
+            }
+            else
+            {
+                print("obstacle in front");
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (ClearToMove(false))
+            {
+                transform.localPosition -= transform.forward * increment;
+            }
+            else
+            {
+                print("obstacle behind");
+            }
+>>>>>>> Stashed changes
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
