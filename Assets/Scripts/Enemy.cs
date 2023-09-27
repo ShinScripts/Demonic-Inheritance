96b856c5aeb96a9f8aa459748b988dec2b4cdc26
Linus Ekberg <103569362+KR0SS0@users.noreply.github.com>
2023-09-27 21:17:22 +0200

    Enemy updates

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,34 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
-{
+public class Enemy : MonoBehaviour {
     public GameObject start;
     public GameObject end;
     public float speed = 2f;
 
-    GameObject target;
+    private GameObject target;
+    private GameObject player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    private NavMeshAgent agent;
+
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+
         transform.position = start.transform.position;
         target = end;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = Vector3.MoveTowards(gameObject.transform.position, target.transform.position, speed * Time.deltaTime);
-        if (gameObject.transform.position == target.transform.position)
-        {
+    void Update() {
+        transform.LookAt(player.transform);
+
+        bool has_hit = Physics.Raycast(transform.position + transform.forward, transform.forward, out RaycastHit hit, 200f);
+
+        if (has_hit && hit.transform.CompareTag("Player")) {
+            agent.SetDestination(player.transform.position);
+            return;
+        } else {
+            agent.SetDestination(target.transform.position);
+        }
+
+        if (Vector3.Distance(transform.position, target.transform.position) < 0.5f) {
             ChangeTargets();
         }
     }
 
-    void ChangeTargets()
-    {
+    void ChangeTargets() {
         target = (target == start) ? end : start;
     }
 }
