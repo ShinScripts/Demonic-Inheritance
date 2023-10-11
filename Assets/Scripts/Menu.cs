0b58ebd89ab7924eb3e8520979c77bf8f8fcca78
Linus Ekberg <103569362+KR0SS0@users.noreply.github.com>
2023-10-11 16:35:20 +0200

    FMOD Supported menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,56 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using FMODUnity;
+using FMOD.Studio;
 
 public class Menu : MonoBehaviour {
     int menuItemSelected = 1;
     int totalItems;
 
-    public AudioClip[] menuItemCips;
-    public AudioSource source;
+    public EventReference[] menuItemEvents;
+    private EventInstance[] menuItemEventInstances;  // Store EventInstances for menu items
 
-    bool hasPlayedCurrentItem = true;
+    private EventInstance currentPlayingEventInstance; // Store the currently playing event
 
     public int sceneIndexToLoad = 1;
 
     [Space(20)]
     [Header("Tutorial Audio")]
-    public AudioClip[] tutorialClips;
+    public EventReference[] tutorialEvents;
+    private EventInstance[] tutorialEventInstances;  // Store EventInstances for tutorial events
 
     private bool isTutorialPlaying = false;
-
-    // Start is called before the first frame update
+    private bool hasPlayedCurrentItem = false;
     private void Start() {
-        totalItems = menuItemCips.Length;
+        totalItems = menuItemEvents.Length;
+
+        // Initialize EventInstances for menu items
+        menuItemEventInstances = new EventInstance[totalItems];
+        for (int i = 0; i < totalItems; i++) {
+            menuItemEventInstances[i] = RuntimeManager.CreateInstance(menuItemEvents[i]);
+        }
     }
 
-    // Update is called once per frame
     void Update() {
-        /*
-        if(Input.GetKeyDown(KeyCode.Escape)) {
-            source.Stop();
-        }
-        */
+        // Navigate menu
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && menuItemSelected != 0) {
+            Debug.Log("up");
 
-        //Navigate menu
-        if(!source.isPlaying) {
-            if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && menuItemSelected != 0) {
-                menuItemSelected -= 1;
-                hasPlayedCurrentItem = false;
+            menuItemSelected -= 1;
+            hasPlayedCurrentItem = false;
+            Debug.Log("up");
 
-            } else if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && menuItemSelected != totalItems - 1) {
-                menuItemSelected += 1;
-                hasPlayedCurrentItem = false;
+            // Stop the currently playing sound
+            if (currentPlayingEventInstance.isValid()) {
+                currentPlayingEventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             }
+        } else if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && menuItemSelected != totalItems - 1) {
+            menuItemSelected += 1;
+            hasPlayedCurrentItem = false;
+            Debug.Log("down");
 
-            if (!hasPlayedCurrentItem) {
-                source.clip = menuItemCips[menuItemSelected];
-                source.PlayOneShot(source.clip);
-                hasPlayedCurrentItem = true;
+            // Stop the currently playing sound
+            if (currentPlayingEventInstance.isValid()) {
+                currentPlayingEventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             }
         }
-      
-        //Select and execute
+
+        if (!hasPlayedCurrentItem) {
+            // Play the FMOD event and store it as the currently playing event
+            menuItemEventInstances[menuItemSelected].start();
+            currentPlayingEventInstance = menuItemEventInstances[menuItemSelected];
+            hasPlayedCurrentItem = true;
+        }
+
+        // Select and execute
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space)) {
             switch (menuItemSelected) {
                 case 0:
@@ -59,9 +72,8 @@ public class Menu : MonoBehaviour {
                     break;
 
                 case 1:
-                    if (!isTutorialPlaying) 
-                    {
-                        StartCoroutine(PlayTutorial());
+                    if (!isTutorialPlaying) {
+                        //play tutorial
                     }
                     break;
 
@@ -70,17 +82,5 @@ public class Menu : MonoBehaviour {
                     break;
             }
         }
-    }
-
-    private IEnumerator PlayTutorial() {
-        isTutorialPlaying = true;
-
-        foreach (var clip in tutorialClips) {
-            source.clip = clip;
-            source.PlayOneShot(source.clip);
-            yield return new WaitForSeconds(source.clip.length + 1f);
-        }
-
-        isTutorialPlaying = false; 
     }
 }
